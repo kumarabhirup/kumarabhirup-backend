b9b4ca2199ac204fc771961623f14318abb38973
Kumar Abhirup <kumar@iqubex.com>
2019-02-06 22:17:16 +0530

    🚀 Deelopment started

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,7 +2,7 @@ const Mutations = {
 
     async createUser(parent, args, context, info){
         const user = await context.db.mutation.createUser({
-            data: { ...args }
+            data: {...args}
         }, info)
         console.log(user)
         return user
