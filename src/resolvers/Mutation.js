fe43e667790b8cd60f2c3e7421dbcd5e0c628eb5
Kumar Abhirup <kumar@iqubex.com>
2019-02-09 14:15:42 +0530

    ✏️ Add SERVER SIDE HUMAN VALIDATION

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -5,44 +5,65 @@ const { validateEmail } = require('../utils')
 
 const Mutations = {
 
-    async sendMessage(parent, args, context, info){
+  async sendMessage(parent, args, context, info){
 
-        const data = { ...args }
+      const data = { ...args }
+      delete data.humanKey
 
-        // Check fullname
-        if(data.fullname.length < 4) {
-          throw new Error(`Fullname so small? 🤔`)
-        }
+      // Check fullname
+      if(args.fullname.length < 4) {
+        throw new Error(`Fullname so small? 🤔`)
+      }
 
-        // Validate email
-        if(!validateEmail(data.email)) {
-          throw new Error(`Please enter your CORRECT EMAIL ADDRESS! 😡`)
-        }
+      // Validate email
+      if(!validateEmail(args.email)) {
+        throw new Error(`Please enter your CORRECT EMAIL ADDRESS! 😡`)
+      }
 
-        // Validate subject
-        if(data.subject.length < 6) {
-          throw new Error(`I hate lazy ppl writing less in subject field!`)
-        }
+      // Validate subject
+      if(args.subject.length < 6) {
+        throw new Error(`I hate lazy ppl writing less in subject field!`)
+      }
 
-        // Validate message
-        if(data.message.length < 9) {
-          throw new Error(`Can you not express yourself longer?`)
-        }
+      // Validate message
+      if(args.message.length < 9) {
+        throw new Error(`Can you not express yourself longer?`)
+      }
 
-        // Validate Phone number
-        const isPhoneValid = await fetch(`http://apilayer.net/api/validate?access_key=${process.env.APILAYER_KEY}&number=${data.phone}`)
+      // Validate Phone number
+      if(args.phone != null) {
+        const isPhoneValid = await fetch(`http://apilayer.net/api/validate?access_key=${process.env.APILAYER_KEY}&number=${args.phone}`)
         .then(res => (res.json()))
         .then(json => (json.valid))
+        .catch(err => { throw new Error(`Error in apilayer API. ${err.message}`) })
 
-        if(data.phone != null && !isPhoneValid){
+        if(args.phone != null && !isPhoneValid){
           throw new Error(`Please write a valid phone number.`)
         }
+      }
 
-        // JUST DO IT!!!
-        const contact = await context.db.mutation.createContact({data}, info)
-        return contact
+      // Validate Human
+      const isHuman = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+        },
+        body: `secret=${process.env.RECAPTCHA_SERVER_KEY}&response=${args.humanKey}`
+      })
+      .then(res => (res.json()))
+      .then(json => (json.success))
+      .catch(err => { throw new Error(`Error in Google Siteverify API. ${err.message}`) })
 
-    }
+      if(args.humanKey == null || !isHuman){
+        throw new Error(`YOU ARE NOT A HUMAN.`)
+      }
+
+      // JUST DO IT!!!
+      const contact = await context.db.mutation.createContact({data}, info)
+      return contact
+
+  }
 
 };
 
