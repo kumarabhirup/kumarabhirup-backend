ca0ed80cdc06cf452c4c7400a481b135a5f1f6d8
Kumar Abhirup <kumar@iqubex.com>
2019-02-12 22:03:44 +0530

    ✏️ Add CORS

--- src/index.js
+++ src/index.js
@@ -18,4 +18,12 @@ const server = new GraphQLServer({
     prisma,
   }),
 })
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: "http://localhost:3006"
+    },
+  },
+  () => console.log(`Server is running on http://localhost:4000`)
+)
